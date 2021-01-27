bff99b9a653850c581a6d0315600c77c5dff3110
tarifkazibd <78075983+tarifkazibd@users.noreply.github.com>
2021-01-27 15:12:38 +0600

    Add files via upload

---
+++ inheritance.php
@@ -0,0 +1,73 @@
+<?php
+
+class calculator {
+public $a;
+public $b;
+public $c;
+public $d;
+
+
+    public function __construct($value1, $value2){
+    $this->a=$value1;
+    $this->b=$value2;
+
+            }
+}
+
+//Inheritance
+class result extends calculator{
+public $v3;
+
+public function allResult(){
+if ($this->v3 == "+") {
+    
+        $this->c = $this->a + $this->b;
+            echo " Addition result is: ". $this->c. "<br>----------------- <br>";
+                
+            }
+
+        elseif ($this->v3== "-") {
+    
+        $this->c = $this->a - $this->b;
+            echo " Subtraction result is: ". $this->c. "<br>----------------- <br>";
+                
+            }
+
+        elseif ($this->v3== "*") {
+    
+        $this->c = $this->a * $this->b;
+            echo " Multiplication result is: ". $this->c. "<br>----------------- <br>";
+                
+            }
+            
+            elseif ($this->v3== "/") {
+    
+        $this->c = $this->a / $this->b;
+            echo " Division result is: ". $this->c. "<br>----------------- <br>";
+                
+            }
+
+        }
+
+}
+
+
+
+
+
+$v1=20;
+$v2=5;
+
+
+$res=new result($v1, $v2);
+$res->v3="-";
+$res->allResult();
+
+
+
+
+
+
+
+
+?>
